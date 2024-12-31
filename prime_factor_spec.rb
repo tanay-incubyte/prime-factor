4268d898b70a736cb524f79d0adf1739659cd6c1
tanay-incubyte <tanay@incubyte.co>
2024-12-31 12:11:02 +0000

    add more tests

--- prime_factor_spec.rb
+++ prime_factor_spec.rb
@@ -10,9 +10,12 @@ RSpec.describe 'Factors of a number' do
 
   context '#prime_factors' do
     it 'returns only prime factors for a number' do
-      expect(prime_factor(2)).to eq([2])      # Prime factors of 2
-      expect(prime_factor(6)).to eq([2, 3])  # Prime factors of 6
-      expect(prime_factor(15)).to eq([3, 5]) # Prime factors of 15
+      expect(prime_factor(2)).to eq([2])     
+      expect(prime_factor(6)).to eq([2, 3]) 
+      expect(prime_factor(15)).to eq([3, 5])
+      expect(prime_factor(30)).to eq([2, 3, 5])
+      expect(prime_factor(100)).to eq([2, 5])
+      expect(prime_factor(555)).to eq([5, 7, 37])
     end
   end
 end
