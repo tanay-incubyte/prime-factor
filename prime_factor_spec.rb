778426a6520c7707edc395f2e418936055b96c4d
tanay-incubyte <tanay@incubyte.co>
2024-12-31 12:35:07 +0000

    update tests with new method name

--- prime_factor_spec.rb
+++ prime_factor_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe 'Factors of a number' do
 
   context '#prime_factors' do
     it 'returns only prime factors for a number' do
-      expect(prime_factor(2)).to eq([2])     
-      expect(prime_factor(6)).to eq([2, 3]) 
-      expect(prime_factor(15)).to eq([3, 5])
-      expect(prime_factor(17)).to eq([17])
-      expect(prime_factor(30)).to eq([2, 3, 5])
-      expect(prime_factor(100)).to eq([2, 5])
-      expect(prime_factor(555)).to eq([3, 5, 37])
-      expect(prime_factor(2457)).to eq([3, 7, 13])
-      expect(prime_factor(30030)).to eq([2, 3, 5, 7, 11, 13])
+      expect(prime_factors(2)).to eq([2])     
+      expect(prime_factors(6)).to eq([2, 3]) 
+      expect(prime_factors(15)).to eq([3, 5])
+      expect(prime_factors(17)).to eq([17])
+      expect(prime_factors(30)).to eq([2, 3, 5])
+      expect(prime_factors(100)).to eq([2, 5])
+      expect(prime_factors(555)).to eq([3, 5, 37])
+      expect(prime_factors(2457)).to eq([3, 7, 13])
+      expect(prime_factors(30030)).to eq([2, 3, 5, 7, 11, 13])
     end
   end
 end
