c6aefefc967340950806a4df0672c66a31e973d7
tanay-incubyte <tanay@incubyte.co>
2024-12-31 11:17:44 +0000

    add tests to check for prime factors only

--- prime_factor_spec.rb
+++ prime_factor_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe 'Factors of a number' do
       expect(all_factors(6)).to eq([1, 2, 3, 6])
     end
   end
+
+  context '#prime_factors' do
+    it 'returns only prime factors for a number' do
+      expect(prime_factor(2)).to eq([2])      # Prime factors of 2
+      expect(prime_factor(6)).to eq([2, 3])  # Prime factors of 6
+      expect(prime_factor(15)).to eq([3, 5]) # Prime factors of 15
+    end
+  end
 end
