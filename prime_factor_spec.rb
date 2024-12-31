5d8679be9da966051c5d3da017a3e04729bb1aa0
tanay-incubyte <tanay@incubyte.co>
2024-12-31 11:06:19 +0000

    add tests to check for all factors of a number

--- prime_factor_spec.rb
+++ prime_factor_spec.rb
@@ -2,6 +2,7 @@ require_relative 'prime_factor'
 
 RSpec.describe '#prime_factor' do
 	it 'returns the factors for a number' do
-		expect(prime_factor(2)).to eq(2)
+		expect(prime_factor(2)).to eq([1,2])
+		expect(prime_factor(6)).to eq([1,2,3,6])
 	end
 end
